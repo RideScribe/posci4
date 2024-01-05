1ee441e3a29e18fb2ef2b76f28e305d32f790f73
FAISAL HALIM <ffaisalhalim@gmail.com>
2024-01-06 06:20:25 +0700

    Fix filter value in penjualan.php

--- app/Views/laporan/penjualan.php
+++ app/Views/laporan/penjualan.php
@@ -8,8 +8,8 @@
             
             <!-- Filter -->
             <form action="" method="get" class="mb-3 d-flex flex-column flex-md-row justify-content-center align-items-center" style="gap: 5px;">
-                <input type="date" name="tanggal" id="tanggal" class="form-control" value="<?= in_array('tanggal', $filter) ? $filter['tanggal'] : '' ?>">
-                <select name="status" id="status" class="form-control custom-select" value="<?= in_array('tanggal', $filter) ? $filter['status'] : '' ?>">
+                <input type="date" name="tanggal" id="tanggal" class="form-control" value="<?= in_array('tanggal', $filter) ? $filter['tanggal'] : null ?>">
+                <select name="status" id="status" class="form-control custom-select" value="<?= in_array('tanggal', $filter) ? $filter['status'] : null ?>">
                     <option <?= (in_array('status', $filter) ? $filter['status'] : '') == '' ? 'selected' : '' ?> value="">-- Status --</option>
                     <option <?= (in_array('status', $filter) ? $filter['status'] : '') == 1 ? 'selected' : '' ?> value="1">Lunas</option>
                     <option <?= (in_array('status', $filter) ? $filter['status'] : '') == 0 ? 'selected' : '' ?> value="0">Belum Lunas</option>
